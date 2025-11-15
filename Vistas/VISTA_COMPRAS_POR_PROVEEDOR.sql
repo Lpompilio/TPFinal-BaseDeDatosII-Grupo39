c0238a2ea11536ce26fc2f43b50726ef85b9cd1b
facualberti <facundo_alberti@yahoo.com.ar>
2025-11-15 20:57:30 -0300

    Vista de compras por proveedor

---
+++ Vistas/VISTA_COMPRAS_POR_PROVEEDOR.sql
@@ -0,0 +1,19 @@
+CREATE VIEW vw_ComprasPorProveedor AS
+SELECT 
+    p.RazonSocial,
+    COUNT(c.IdCompra) AS CantidadCompras,
+    SUM(c.Total) AS TotalComprado
+FROM Proveedor p
+INNER JOIN Compra c ON p.IdProveedor = c.IdProveedor
+GROUP BY p.RazonSocial;
+GO
+
+-- Para Mostrar la Vista
+
+SELECT * FROM vw_ComprasPorProveedor;
+
+-- Para filtrar datos dentro de la vista
+
+SELECT *
+FROM vw_ComprasPorProveedor
+WHERE RazonSocial = 'Proveedor XYZ';
